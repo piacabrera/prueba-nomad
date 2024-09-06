3c0903a5aafd6de7d026adde65447fbfb5e668ad
Pia Cabrera <mara.cabrera@uc.cl>
2024-09-06 13:02:28 -0400

    feat(utils): find cart products

---
+++ src/utils/utils.js
@@ -0,0 +1,28 @@
+
+
+function findCartProducts(allProducts, cartItems) {
+  return cartResult = cartItems.map((item) => {
+    const product = allProducts.find((product) => product.id.toString() === item.productId);
+    if (!product) {
+      return {
+        ...item,
+        error: 'Producto no encontrado'
+      };
+    }  
+    const { title, stock, rating } = product;
+    const realStock = Math.floor(stock / rating);
+    const itemResult = {
+      productId: item.productId,
+      title,
+      price: item.price,
+      totalDiscount: item.discount * item.quantity,
+      quantity: item.quantity,
+      stock,
+      rating,
+      realStock,
+    };
+    return itemResult;
+  });
+}
+
+module.exports = { findCartProducts };
