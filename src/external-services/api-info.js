bc4f7409cca294f2c0f21b10804e87d6733ad924
Pia Cabrera <mara.cabrera@uc.cl>
2024-09-06 12:35:50 -0400

    feat(externalServices): get external API info

---
+++ src/external-services/api-info.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+async function getAllProducts() {
+    let currentPage = 1;
+    let allProducts = [];
+    const pageSize = 10;
+  
+    try {
+      while (true) {
+        const response = await axios.get(`https://dummyjson.com/products?limit=${pageSize}&skip=${(currentPage - 1) * pageSize}`);
+        const products = response.data.products;
+  
+        allProducts = [...allProducts, ...products];
+  
+        if (products.length < pageSize) {
+          break;
+        }
+
+        currentPage++;
+      }
+    } catch (error) {
+      console.error('Error al obtener productos de dummyjson:', error.message);
+      return [];
+    }
+    return allProducts;
+}
+(async () => {
+  const products = await getAllProducts();
+  console.log(products);
+})();
+module.exports = { getAllProducts };
+
