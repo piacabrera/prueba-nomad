3e75b814fa85e76b1a53064294fa05d6ea59c03d
Pia Cabrera <mara.cabrera@uc.cl>
2024-09-06 12:36:56 -0400

    feat(): basic express api

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const audit = require('express-requests-logger');
+const apiRoutes = require('./src/endpoints/api');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(audit(), bodyParser.json());
+
+app.use('/api', apiRoutes);
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
