855e4658ca1abfd72745070e125fa84d728f967c
Pia Cabrera <mara.cabrera@uc.cl>
2024-09-07 12:47:18 -0400

    feat(): add path

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const audit = require('express-requests-logger');
+const path = require('path');  // Import 'path' module for constructing file paths
 const apiRoutes = require('./src/endpoints/api');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;  // Use a default port if not specified
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Middleware for logging requests
+app.use(audit());
+
+// API routes
 app.use('/api', apiRoutes);
 
-app.use(express.static('../frontend/build'));
+// Serve static files from the React frontend build directory
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+// Catch-all handler to serve the React app for any route not handled by the API
 app.get('*', (req, res) => {
-  res.sendFile('../frontend/build', 'index.html');
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
